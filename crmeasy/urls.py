4491d0c21b63d6e40d22c635955a388f327299ac
tuangit2017 <tuanallejo12@gmail.com>
2017-05-03 09:25:38 -0700

    add new project crmeasy

---
+++ crmeasy/urls.py
@@ -0,0 +1,19 @@
+from django.conf import settings   
+from django.conf.urls.static import static	
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'crmeasy.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+	url(r'^crmapp/', include('crmapp.urls')),
+)
+
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+  #  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
